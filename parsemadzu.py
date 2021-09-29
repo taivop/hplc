b4c7e687f969369c872531671f034b73b3c41e7d
taivop <taivo@pungas.ee>
2021-09-29 11:41:46 +0300

    pre-commit

--- parsemadzu.py
+++ parsemadzu.py
@@ -1,9 +1,11 @@
 import re
-import pandas as pd
 from io import StringIO
 from typing import Optional
 
+import pandas as pd
+
 re_sections = re.compile(r"\[(.*)\]")
+
 
 def parse_sections(file_path: str) -> dict:
     """Parse a Shimadzu ASCII-export file into sections."""
@@ -17,18 +19,24 @@ def parse_sections(file_path: str) -> dict:
 
     section_names = [split[i] for i in range(1, len(split), 2)]
     section_contents = [split[i] for i in range(2, len(split), 2)]
-    
+
     sections = {name: content for name, content in zip(section_names, section_contents)}
     return sections
 
+
 def parse_meta(sections: dict, section_name: str, nrows: int) -> dict:
     """Parse the metadata in a section as keys-values."""
-    meta_table = pd.read_table(StringIO(sections[section_name]), nrows=nrows, header=None)
+    meta_table = pd.read_table(
+        StringIO(sections[section_name]), nrows=nrows, header=None
+    )
     meta = {row[0]: row[1] for _, row in meta_table.iterrows()}
-    
+
     return meta
 
-def parse_table(sections: dict, section_name: str, skiprows: int = 1) -> Optional[pd.DataFrame]:
+
+def parse_table(
+    sections: dict, section_name: str, skiprows: int = 1
+) -> Optional[pd.DataFrame]:
     """Parse the data in a section as a table."""
     table_str = sections[section_name]
 
@@ -37,49 +45,66 @@ def parse_table(sections: dict, section_name: str, skiprows: int = 1) -> Optiona
 
     if num_lines <= 1:
         return None
-    
+
     return pd.read_table(StringIO(table_str), header=1, skiprows=skiprows)
+
 
 def get_peak_table(sections: dict, detector: str = "A") -> Optional[pd.DataFrame]:
     section_name = f"Peak Table(Detector {detector})"
     meta = parse_meta(sections, section_name, 1)
     table = parse_table(sections, section_name, skiprows=1)
 
-    assert int(meta["# of Peaks"]) == table.shape[0], "Declared number of peaks and table size differ"
-    
+    assert (
+        int(meta["# of Peaks"]) == table.shape[0]
+    ), "Declared number of peaks and table size differ"
+
     return table
+
 
 def get_compound_table(sections: dict, detector: str = "A") -> Optional[pd.DataFrame]:
     section_name = f"Compound Results(Detector {detector})"
     meta = parse_meta(sections, section_name, 1)
     table = parse_table(sections, section_name, skiprows=1)
 
-    assert int(meta["# of IDs"]) == table.shape[0], "Declared number of compounds and table size differ"
-    
+    assert (
+        int(meta["# of IDs"]) == table.shape[0]
+    ), "Declared number of compounds and table size differ"
+
     return table
 
-def get_chromatogram_table(sections: dict, detector: str = "A", channel: int = 1) -> Optional[pd.DataFrame]:
+
+def get_chromatogram_table(
+    sections: dict, detector: str = "A", channel: int = 1
+) -> Optional[pd.DataFrame]:
     section_name = f"LC Chromatogram(Detector {detector}-Ch{channel})"
-    
-    meta = parse_meta(sections, section_name, 6)    
+
+    meta = parse_meta(sections, section_name, 6)
     table = parse_table(sections, section_name, skiprows=6)
-    
+
     # Convert intensity values into what they are supposed to be
     table["Value (mV)"] = table["Intensity"] * float(meta["Intensity Multiplier"])
-    
-    assert meta["Intensity Units"] == "mV", f"Assumed intensity units in mV but got {meta['Intensity Units']}"
-    assert int(meta["# of Points"]) == table.shape[0], "Declared number of points and table size differ"
+
+    assert (
+        meta["Intensity Units"] == "mV"
+    ), f"Assumed intensity units in mV but got {meta['Intensity Units']}"
+    assert (
+        int(meta["# of Points"]) == table.shape[0]
+    ), "Declared number of points and table size differ"
 
     return table
+
 
 def get_header(sections: dict) -> dict:
     return parse_meta(sections, "Header", nrows=None)
 
+
 def get_file_information(sections: dict) -> dict:
     return parse_meta(sections, "File Information", nrows=None)
 
+
 def get_original_files(sections: dict) -> dict:
     return parse_meta(sections, "Original Files", nrows=None)
+
 
 def get_sample_information(sections: dict) -> dict:
     return parse_meta(sections, "Sample Information", nrows=None)
